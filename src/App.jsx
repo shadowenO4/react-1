f886363ef69bfc11c266ef53712d37c208013c15
shadowenO4 <akramalsadi04@gmail.com>
2025-10-31 09:13:20 +0200

    navbar

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Navbar from './component/Navbar';
+import Hero from './component/Hero';
+import Portfolio from './component/Portfolio';
+import About from './component/About';
+import Contact from './component/contact';
+import Footer from './component/Footer';
+import './styles/style.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      
+    </div>
+  );
+}
+
+export default App;
